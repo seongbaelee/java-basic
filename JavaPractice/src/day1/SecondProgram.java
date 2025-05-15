dfee9702fc0557bee42c1ac714a5eea511799870
seongbae <seongbaelee94@gmail.com>
2025-05-15 05:03:51 -0400

    Day 1: Basic print statements

---
+++ JavaPractice/src/day1/SecondProgram.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome");
+		System.out.println(1+2);
+		System.out.println("1+2");
+		System.out.println("welcome"+123);
+	}
+
+}
