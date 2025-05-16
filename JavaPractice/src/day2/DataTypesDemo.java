3e1c5d368a074aada72ba8d1e818e21d396ce177
seongbae <seongbaelee94@gmail.com>
2025-05-16 01:15:57 -0400

    Day2: Variables and DataTypes

---
+++ JavaPractice/src/day2/DataTypesDemo.java
@@ -0,0 +1,41 @@
+package day2;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		//Numeric data types
+		int a = 100, b = 200;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+
+		byte by=120;
+		System.out.println(by);
+		
+		short sh=250;
+		System.out.println(sh);
+		
+		long l = 123456789054054153L; //Literal is needed at the end of the value when lengthy
+		System.out.println(l);
+		
+		//Decimal number 
+		float price = 150.5F; // Literal is needed at the end of the value
+		System.out.println(price);
+		
+		double item_price = 145.32154;
+		System.out.println(item_price);
+		
+		//Character
+		char grad = 'B';
+		System.out.println(grad);
+		
+		//Boolean
+		boolean bool = true;
+		System.out.println(bool);
+		
+		//String
+		String name = "John";
+	}
+
+}
---
+++ JavaPractice/src/day2/VariablesDemo.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// Example 1
+//		int a; // declaration
+//		a = 100; // assignment
+
+		// Example 2
+//		int a = 100; // declaration + assignment
+//		a = 500;
+//		System.out.println(a);
+
+		// Example 3 - if all the variables are belongs to same data type
+//		int a = 100;
+//		int b = 200;
+//		int c = 300;
+
+		// Example 3 - 1
+//		int a, b, c;
+//		a = 100;
+//		b = 200;
+//		c = 300;
+		
+		// Example 3 - 2
+		int a = 100, b = 200, c = 300;
+
+		System.out.println("the value of a is " + a);
+		System.out.println("the value of b is " + b);
+		System.out.println("the value of c is " + c);
+		
+		System.out.println(a + ""  + b + "" + c);
+	}
+
+}
